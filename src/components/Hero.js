15628734fc3782a36b4843603d469e547e89bafb
Mahdi <mahdi.sundarani@gmail.com>
2024-03-02 12:12:36 +0530

    data-finance

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,20 +1,36 @@
-import React from 'react'
+import React from "react";
 import { ReactTyped } from "react-typed";
 
 const Hero = () => {
-    return (
-        <div className='text-white'>
-            <div className='max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center'>
-                <p className='text-[#00df9a] font-bold p-2'>GROWING WITH DATA ANALYTICS</p>
-                <h1 className='md:text-7xl sm:text-6xl text-4xl font-bold md:py-6'>Grow with data</h1>
-                <div>
-                    <p className='md:text-5xl sm:text-4xl text-xl font-bold '>Fast, flexible financing for</p>
-                    <ReactTyped strings={['BTB', 'BTC', 'SASS']} typeSpeed={120} backSpeed={140} loop />
-                </div>
-            </div>
+  return (
+    <div className="text-white">
+      <div className="max-w-[800px] mt-[-96px] w-full h-screen mx-auto text-center flex flex-col justify-center">
+        <p className="text-[#00df9a] font-bold p-2">
+          GROWING WITH DATA ANALYTICS
+        </p>
+        <h1 className="md:text-7xl sm:text-6xl text-4xl font-bold md:py-6">
+          Grow with data
+        </h1>
+        <div className="flex justify-center items-center">
+          <p className="md:text-5xl sm:text-4xl py-4 text-xl font-bold ">
+            Fast, flexible financing for
+          </p>
+          <ReactTyped
+            className="md:text-5xl sm:text-4xl md:pl-4 text-xl font-bold pl-2"
+            strings={["BTB", "BTC", "SASS"]}
+            typeSpeed={120}
+            backSpeed={140}
+            loop
+          />
         </div>
-    )
-}
+        <p className="md:text-2xl text-xl font-bold text-gray-500">
+          Monitor your data analytics to increase revenue fro BTB.BTC & SASS
+          platforms
+        </p>
+        <button className="bg-[#00df9a] w-[200px] rounded-md mx-auto my-6 py-3 text-black font-medium ">Get Started</button>
+      </div>
+    </div>
+  );
+};
 
-export default Hero
-
+export default Hero;
